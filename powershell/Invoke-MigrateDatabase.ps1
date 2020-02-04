ab3e57fc071de3e0529e70a7115c52f79107422d
Florian Carrier <Akaizoku@users.noreply.github.com>
2020-02-04 14:37:21 +0000

    Create Invoke-MigrateDatabase.ps1

---
+++ powershell/Invoke-MigrateDatabase.ps1
@@ -0,0 +1,69 @@
+function Invoke-MigrateDatabase {
+  <#
+    .SYNOPSIS
+    Migrate RiskPro database
+
+    .DESCRIPTION
+    Upgrade the database of OneSumX for Risk Management
+
+    .PARAMETER Properties
+    The properties parameter corresponds to the configuration of the application.
+
+    .EXAMPLE
+    Invoke-MigrateDatabase -Properties $Properties
+
+    .NOTES
+    File name:      Invoke-MigrateDatabase.ps1
+    Author:         Florian Carrier
+    Creation date:  25/11/2019
+    Last modified:  22/01/2020
+  #>
+  [CmdletBinding (
+    SupportsShouldProcess = $true
+  )]
+  Param (
+    [Parameter (
+      Position    = 1,
+      Mandatory   = $true,
+      HelpMessage = "Properties"
+    )]
+    [ValidateNotNullOrEmpty ()]
+    [System.Collections.Specialized.OrderedDictionary]
+    $Properties
+  )
+  Begin {
+    # Get global preference variables
+    Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+    # Define partinioning flag
+    if ($Properties.EnablePartitioning -eq $true) {
+      $Partitioning = $true
+    } else {
+      $Partitioning = $false
+    }
+  }
+  Process {
+    Write-Log -Type "INFO" -Object "Migrating database"
+    $Migration = Invoke-MigratorTool -JavaPath $Properties.JavaPath -MigratorTool $Properties.MigratorToolPath -DatabaseVendor $Properties.DatabaseDriver -DatabaseHost $Properties.DatabaseHost -DatabasePort $Properties.DatabasePort -DatabaseInstance $Properties.DatabaseInstance -Version $Properties.RiskProVersion -Credentials $Properties.RPDBCredentials -Log $Properties.MigrationLog -Partitioning:$Partitioning -Backup -Silent
+    # Check outcome
+    if (Test-MigratorToolOutcome -Log $Migration) {
+      Write-Log -Type "CHECK" -Object "Database successfully migrated"
+      return $true
+    } else {
+      $ErrorPatterns = @(
+        [RegEx]::New('(?<=(\[ERROR\]).+(com\.frsglobal\.migrator\.console\.Launcher -- )).+'),
+        [RegEx]::New('(?<=(\[SEVERE\]).+(com\.frsglobal\.migrator\.console\.Launcher -- )).+')
+      )
+      # Retrieve errors
+      foreach ($Line in ($Migration -split "`n")) {
+        foreach ($ErrorPattern in $ErrorPatterns) {
+          $ErrorMessage = Select-String -InputObject $Line -Pattern $ErrorPattern | Select-Object -ExpandProperty "Matches" | Select-Object -ExpandProperty "Value"
+          if ($ErrorMessage) {
+            Write-Log -Type "WARN" -Object $ErrorMessage
+          }
+        }
+      }
+      Write-Log -Type "ERROR" -Object "Database migration failed"
+      return $false
+    }
+  }
+}
